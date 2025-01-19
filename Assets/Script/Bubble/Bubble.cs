7208e338c7f5bfbc176d4f031c4da212ffe764f1
日向晚晚子 <2358783122@qq.com>
2025-01-19 14:23:19 +0800

    泡泡最终处理

--- Assets/Script/Bubble/Bubble.cs
+++ Assets/Script/Bubble/Bubble.cs
@@ -45,7 +45,7 @@ public class Bubble : MonoBehaviour
 
     private void GetSpeed(float blowTime)
     {
-        speed =(0.3f+ blowTime*2)/4.3f ;
+        speed =(0.3f+ blowTime*2)/4.5f ;
     }
 
     private void GetScore(float blowTime)
